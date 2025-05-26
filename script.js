3dcd10e02ba5302abd45b11c92f31e4d706fb43f
Mike <mpone1717@gmail.com>
2025-05-26 03:32:38 -0400

    created play round function determine the logic of rps. Which play will win the round based on their choice

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function getComputerChoice () {
     return randomIndex
 }
 
-console.log(getComputerChoice());
+
 
 let humanChoice = prompt("choose rock, paper, scissors")
 function getHumanChoice (){
-    if(humanChoice === 'rock'){
+     if(humanChoice === 'rock'){
         return 'rock'
     }else if(humanChoice === 'paper'){
         return 'paper'
@@ -17,4 +17,32 @@ function getHumanChoice (){
     }
 }
 
-console.log(getHumanChoice());
+
+
+let humanScore = 0
+let cpuScore = 0
+
+function playRound (humanSelect , cpuSelect){
+    
+    
+    if(humanSelect === cpuSelect){
+        return "tie"
+
+    } else if((humanSelect === 'rock' && cpuSelect === 'paper') ||
+              (humanSelect === 'paper' && cpuSelect === 'scissors')||
+         (humanSelect === 'scissors' && cpuSelect === 'rock')){
+            return `you  lose - ${cpuChoice} beats ${humanPick}`
+            cpuScore++
+           
+        } else {
+          return `you win - ${humanPick} beats ${cpuChoice}`
+          humanScore++
+          }
+        }
+       let cpuChoice = getComputerChoice ()
+       let humanPick = getHumanChoice ()
+        
+        console.log(playRound(humanPick, cpuChoice));
+        
+
+
